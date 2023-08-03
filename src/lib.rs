4b2e3ec83aaa2e0cdbc7491b30260fc6dc18ce88
Pedro Ulrich <pedroheringer2020@gmail.com>
2023-08-03 16:12:55 -0300

    moved handle_connection to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 use std::{
+    fs,
+    io::prelude::*,
+    net::TcpStream,
     sync::{
         mpsc::{self},
         Arc, Mutex,
     },
     thread,
+    time::Duration,
 };
 
 pub struct ThreadPool {
@@ -82,4 +86,33 @@ impl Worker {
             thread: Some(thread),
         }
     }
+}
+
+pub fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
